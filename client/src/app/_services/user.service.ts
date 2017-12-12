0b9ff4659abacafd9c5580bb04b346e7175df9ff
Nivesh Varma <nvarma@ncsu.edu>
2017-12-12 03:58:06 -0500

    fixed user model

--- client/src/app/_services/user.service.ts
+++ client/src/app/_services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 
-// import { User } from '../_models/index';
+import { User } from '../_models/index';
 
 @Injectable()
 export class UserService {
