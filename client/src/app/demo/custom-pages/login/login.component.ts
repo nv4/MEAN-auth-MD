ce5d445cf1a4f6523dd86e1dcd5d80610103fd8e
Nivesh Varma <nvarma@ncsu.edu>
2017-12-12 06:33:12 -0500

    implemented 'remember me' checkbox

--- client/src/app/demo/custom-pages/login/login.component.ts
+++ client/src/app/demo/custom-pages/login/login.component.ts
@@ -19,6 +19,8 @@ export class LoginComponent implements OnInit {
   loading = false;
   returnUrl: string;
 
+  remembered = false;
+
   // email: string;
   // password: string;
 
@@ -30,8 +32,10 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    // reset login status
-    this.authenticationService.logout();
+    if(!this.remembered) {
+      // reset login status
+      this.authenticationService.logout();
+    }
 
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
