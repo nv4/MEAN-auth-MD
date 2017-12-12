46e003cc7d3c118911f60586df05b887603098f8
Nivesh Varma <nvarma@ncsu.edu>
2017-12-12 06:11:24 -0500

    implemented login component module

--- client/src/app/demo/custom-pages/login/login.component.ts
+++ client/src/app/demo/custom-pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router, ActivatedRoute} from "@angular/router";
 import {fadeInAnimation} from "../../../route.animation";
 
 import {AuthenticationService} from '../../../_services/index';
+import {MdSnackBar} from '@angular/material';
 
 @Component({
   selector: 'ms-login',
@@ -14,19 +15,46 @@ import {AuthenticationService} from '../../../_services/index';
   animations: [ fadeInAnimation ]
 })
 export class LoginComponent implements OnInit {
+  model: any = {};
+  loading = false;
+  returnUrl: string;
 
-  email: string;
-  password: string;
+  // email: string;
+  // password: string;
 
   constructor(
-    private router: Router
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private snackBar: MdSnackBar
   ) { }
 
   ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
   login() {
-    this.router.navigate(['/']);
+    // this.router.navigate(['/']);
+    this.loading = true;
+    this.authenticationService.login(this.model.username, this.model.password)
+      .subscribe(
+        data => {
+          this.snackBar.open('You have successfully logged in!', 'Close', {
+            duration: 7000
+          });
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.snackBar.open(error, 'Close', {
+            duration: 7000
+          });
+          this.loading = false;
+        }
+      );
   }
 
 }
